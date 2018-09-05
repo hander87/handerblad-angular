ff81a17f730be44aec5161d3c672670e901dd7e7
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-09-06 00:40:30 +0200

    Api Search Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AnalogclockComponent } from './plugins/analogclock/analogclock.component';
@@ -12,8 +20,12 @@ import { ExperienceComponent } from './layout/experience/experience.component';
 import { CoursesComponent } from './layout/courses/courses.component';
 import { SideprojectsComponent } from './layout/sideprojects/sideprojects.component';
 import { FooterComponent } from './layout/footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ApisearchComponent } from './pages/apisearch/apisearch.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'apisearch', component: ApisearchComponent }
+];
 
 @NgModule({
   declarations: [
@@ -26,12 +38,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ExperienceComponent,
     CoursesComponent,
     SideprojectsComponent,
-    FooterComponent
+    FooterComponent,
+    ApisearchComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpModule
+    HttpModule,
+    MatCardModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
